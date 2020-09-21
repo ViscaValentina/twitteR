77b48e0f54d30f8952455b7e953947601eafa62b
ViscaValentina <61353930+ViscaValentina@users.noreply.github.com>
2020-09-21 16:08:46 +0700

    Update twitteR.R

--- twitteR.R
+++ twitteR.R
@@ -1,3 +1,5 @@
+#insert token given from twitter dev app
+
 consumer_key <- '.....'
 consumer_secret <- '......'
 
@@ -5,18 +7,27 @@ access_token <- '......'
 access_secret <- '.....'
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
+#choose yes
+
 1
 
+#set what to search on twitter (keyword, hashtags, account, volume, period)
+
 tweets <- searchTwitter("keyword or hashtag", n=80, since='2020-05-01')
-tweets 
+tweets
+
+#classify the tweets
 
 class(tweets)
 tweets_text <- sapply(tweets, function(x) x$getText())
 str(tweets_text)
 
-
+#turn the tweets into corpus
+                      
 tweets_corpus <- Corpus(VectorSource(tweets_text))
 tweets_corpus
+                      
+#clean up the corpus
 
 tweets_clean <- tm_map(tweets_corpus, function(x) iconv(enc2utf8(x), sub = "byte"))
 tweets_clean <- tm_map(tweets_corpus, content_transformer(function(x)    iconv(enc2utf8(x), sub = "bytes")))
